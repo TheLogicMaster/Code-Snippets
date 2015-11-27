978540de52283c9be505a27c1a28eeb4f26a17ce
justinmarentette11 <justinmarentette11@gmail.com>
2015-11-27 16:20:37 -0500

    Copied stuff
    Copied stuff

---
+++ src/Window.java
@@ -0,0 +1,21 @@
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Window extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+	
+	public Window() {
+		this.setName("Tower Defense");
+		this.setSize(800, 800);
+		this.setLocation(100, 100);
+		this.setVisible(true);
+		
+		JPanel panel = new JPanel();
+		panel.setSize(700,700);
+		this.add(panel);
+	}
+	
+}
