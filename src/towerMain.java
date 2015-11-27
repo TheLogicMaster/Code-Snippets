8eab2dd9bbad940d3dd5859b2f3d71644958e2ff
justinmarentette11 <justinmarentette11@gmail.com>
2015-11-27 16:38:41 -0500

    Deleted Spamming Text and unneccisary imports
    Deleted Spamming Text and unneccisary imports

--- src/towerMain.java
+++ src/towerMain.java
@@ -4,11 +4,14 @@ public class towerMain {
 	public static void main(String[] args) {
 		int gameLoop=1;
 		long nanoTime=System.nanoTime();
-		
+	
 		while(gameLoop==1){
-		if (System.nanoTime()/Math.pow(10,9)-nanoTime/Math.pow(10,9)>=1/60){			
-		nanoTime=System.nanoTime();
-		System.out.println("1 second has elapsed");
+			if (System.nanoTime()/Math.pow(10,9)-nanoTime/Math.pow(10,9)>=1/60){			
+			nanoTime=System.nanoTime();
+			
+			
+			
+			
 		}
 		//System.out.println(System.nanoTime()/Math.pow(10,9)+":"+(nanoTime/Math.pow(10,9)));
 	}
