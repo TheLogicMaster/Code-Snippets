fe48ecff27d745f3b05e0c5f25088d41b18de3c8
Dactaxx <dmpribak@gmail.com>
2015-11-27 20:02:26 -0500

    Huge loop change

--- src/DrawGraphics.java
+++ src/DrawGraphics.java
@@ -1,11 +1,24 @@
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
 public class DrawGraphics extends JPanel {
 	private static final long serialVersionUID = -9175286406838649119L;
-
+	public int x = 0;
+	public int y = 0;
 	public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.setColor(new Color(0, 0, 0));
+		tick(g2d);
+		
+	}
+	
+	public void tick(Graphics2D g2d) {
+		x += 5;
+		y += 5;
+		g2d.fillRect(x, y, x + 5, y + 5);
 		
 	}
 	
--- src/towerMain.java
+++ src/towerMain.java
@@ -1,34 +1,41 @@
+import java.awt.Graphics2D;
 
-public class towerMain {
-
+public class towerMain implements Runnable {
+	public boolean running = true;
 	public static void main(String[] args) {
 		CreateWindow createwindow = new CreateWindow();
 		createwindow.makeWindow();
-		int gameLoop=1;
-		long nanoTime=System.nanoTime();
+		
+		start();
 	
-		while(gameLoop==1){
-			if (System.nanoTime()/Math.pow(10,9)-nanoTime/Math.pow(10,9)>=1/60){			
-			nanoTime=System.nanoTime();
-			
-<<<<<<< HEAD
-			f
-			
-=======
-			
-<<<<<<< HEAD
-			l
->>>>>>> origin/master
-=======
-			
->>>>>>> origin/master
-			
-		}
+		
 		//System.out.println(System.nanoTime()/Math.pow(10,9)+":"+(nanoTime/Math.pow(10,9)));
 	}
 		
+	public synchronized static void start() {
+		(new Thread(new towerMain())).start();
 		
+	}
+		
+
+	public void run() {
+		loop();
 		
 	}
 
+	public void loop() {
+		DrawGraphics drawgraphics = new DrawGraphics();
+		long now = System.currentTimeMillis();
+		while(running == true){
+			if (System.nanoTime() / 1000 - now / 1000 >= 1/60){
+			drawgraphics.tick((Graphics2D)drawgraphics.getGraphics());
+			System.out.println("test");
+			now = System.nanoTime();
+
+			}
+		
+		}
+		
+	}
+	
 }
