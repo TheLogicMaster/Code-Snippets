29bc88b82d8dfb6c754924b69bcf41092193b71c
justinmarentette11 <justinmarentette11@gmail.com>
2015-11-28 14:21:02 -0500

    More work on curves, initiated pathGenerator

--- src/towerMain.java
+++ src/towerMain.java
@@ -1,36 +1,39 @@
 
-public class towerMain implements Runnable {	
+public class towerMain implements Runnable {
 	static DrawGraphics drawgraphics = new DrawGraphics();
 	static CreateWindow createwindow = new CreateWindow();
 	boolean running = true;
+
 	public static void main(String[] args) {
 		createwindow.makeWindow();
-		
+
 		start();
-		
+
 	}
-	
+
 	public synchronized static void start() {
 		(new Thread(new towerMain())).start();
 	}
-	
+
 	@Override
 	public void run() {
 		long now = System.currentTimeMillis();
-		while(running == true){
-			if (System.nanoTime() / 1000 - now / 1000 >= 1/60) {
-			tick();
-			now = System.nanoTime();
+		pathGenerator pathgenerator = new pathGenerator();
+		pathgenerator.curveGen(50, 50, 100, 50, 100, 100, 30);
+		while (running == true) {
+			if (System.nanoTime() / 1000 - now / 1000 >= 1 / 60) {
+				tick();
+				now = System.nanoTime();
 
 			}
-			
+
 		}
-		
+
 	}
-		
+
 	public void tick() {
 		drawgraphics.tick();
-		
+
 	}
-	
+
 }
