a89ce366263887e07abc51e4cda3f31c33e90141
Dactaxx <dmpribak@gmail.com>
2015-11-28 10:41:46 -0500

    Fixed loop w/out paint

--- src/DrawGraphics.java
+++ src/DrawGraphics.java
@@ -1,42 +1,27 @@
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-import javax.swing.JPanel;
 
-
-public class DrawGraphics extends JPanel {
+public class DrawGraphics extends Canvas {
 	private static final long serialVersionUID = 3848437928663433161L;
 	
 	int x = 0;
 	int y = 0;
-	boolean running = true;
+
 	
 	public void paintComponent(Graphics2D g2d) {
-		long now = System.currentTimeMillis();
-		while(running == true){
-			if (System.nanoTime() / 1000 - now / 1000 >= 1/60) {
-			tick();
-			render(g2d);
-			System.out.println("test");
-			now = System.nanoTime();
-
-			}
-			
-		}
+		g2d.setColor(new Color(0, 0, 0));
+		g2d.fillRect(x, y, 5, 5);
+		
 		
 	}
 	
 	
-	public void render(Graphics2D g2d) {
-		g2d.setColor(new Color(0, 0, 0));
-		g2d.fillRect(x, y, 5, 5);
-		g2d.fillRect(0, 0, 500, 500);
-		System.out.println("test");
-	}
-	
 	public void tick() {
-		x = (int)(Math.random() * 100);
-		y = (int)(Math.random() * 100);
+		x = (int)(Math.random() * 1000);
+		y = (int)(Math.random() * 1000);
+
 		
 	}
 	
--- src/towerMain.java
+++ src/towerMain.java
@@ -1,11 +1,37 @@
 
-public class towerMain {
+public class towerMain implements Runnable {	
 	DrawGraphics drawgraphics = new DrawGraphics();
+	boolean running = true;
 	public static void main(String[] args) {
 		CreateWindow createwindow = new CreateWindow();
 		createwindow.makeWindow();
 		
+		start();
 		//System.out.println(System.nanoTime()/Math.pow(10,9)+":"+(nanoTime/Math.pow(10,9)));
 	}
+	
+	public synchronized static void start() {
+		(new Thread(new towerMain())).start();
+	}
+	
+	@Override
+	public void run() {
+		long now = System.currentTimeMillis();
+		while(running == true){
+			if (System.nanoTime() / 1000 - now / 1000 >= 1/60) {
+			tick();
+			now = System.nanoTime();
+
+			}
+			
+		}
 		
+	}
+		
+	public void tick() {
+		drawgraphics.tick();
+		drawgraphics.repaint();
+		
+	}
+	
 }
